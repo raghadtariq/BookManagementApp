c5835fbeabd6cf7996b22980111debbca8374be1
Raghad <raghaddrabee@gmail.com>
2023-08-20 15:16:49 -0400

    add systemd.service

--- index.ts
+++ index.ts
@@ -2,13 +2,18 @@ import express from "express";
 import bookRouter from "./routers/book.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
-  res.send("Server UP!");
-});
+  res.send("Hello world")
+})
+
+app.get("/health", function (req, res) {
+	res.sendStatus(200);
+})
 
 app.use("/books", bookRouter)
 
